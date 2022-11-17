322b8b092768b1692755b95a174f4b3823de1c6b
ArisDelaRea <113969302+apdelarea@users.noreply.github.com>
2022-11-17 10:37:47 +0800

    Create main.py

---
+++ TelegramToGmail/main.py
@@ -0,0 +1,303 @@
+#receive messages from telegram and forward to email
+#this will run in cron
+
+from flask import Flask, request, Response
+import requests
+import json
+import smtplib
+import re
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+# token that we get from the BotFather
+TOKEN = "5746068823:AAEiok7m-OTfOUsk9wiOflA52l9EvHW9cc8"
+
+app = Flask(__name__)
+
+def getSubject(mcontent):
+  subject = str(mcontent)
+  subject = subject[subject.index('Subject: ')+9:subject.index('Message:')-2]
+ 
+  return subject
+  
+
+def send_email(chatdata, txt):
+  try:
+    mail_content = str(chatdata["message"]["reply_to_message"])
+
+    print(chatdata["message"]["from"]["first_name"])
+
+    #for key in mail_content:
+    #   print(key, ' : ', mail_content[key], "/n")
+    
+    #The mail addresses and password
+    sender_address = 'aris.chatbot@gmail.com'
+    sender_pass = ' ' #enter password here
+    receiver_address = (((str(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", mail_content))).replace("[", "")).replace("]", "")).replace("'", "")
+
+    #print(receiver_address)
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = getSubject(mail_content)   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(txt + " from: " + chatdata["message"]["from"]["first_name"], 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
+  except Exception as e:
+    print(e)
+
+# Reading the JSON message when the user send any type of file to the bot and extracting the chat id of the user and the file id for the file that user send to the bot
+def tel_parse_get_message(message):
+    print("message-->", message)
+
+    try:  # if the file is an image
+        g_chat_id = message['message']['chat']['id']
+        g_file_id = message['message']['photo'][0]['file_id']
+        print("g_chat_id-->", g_chat_id)
+        print("g_image_id-->", g_file_id)
+
+        return g_file_id
+    except:
+        try:  # if the file is a video
+            g_chat_id = message['message']['chat']['id']
+            g_file_id = message['message']['video']['file_id']
+            print("g_chat_id-->", g_chat_id)
+            print("g_video_id-->", g_file_id)
+
+            return g_file_id
+        except:
+            try:  # if the file is an audio
+                g_chat_id = message['message']['chat']['id']
+                g_file_id = message['message']['audio']['file_id']
+                print("g_chat_id-->", g_chat_id)
+                print("g_audio_id-->", g_file_id)
+
+                return g_file_id
+            except:
+                try:  # if the file is a document
+                    g_chat_id = message['message']['chat']['id']
+                    g_file_id = message['message']['document']['file_id']
+                    print("g_chat_id-->", g_chat_id)
+                    print("g_file_id-->", g_file_id)
+
+                    return g_file_id
+                except:
+                    print("NO file found found-->>")
+
+
+# Reading the JSON format when we send the text message and extracting the chat id of the user and the text that user send to the bot
+def tel_parse_message(message):
+    print("message-->", message)
+    try:
+        chat_id = message['message']['chat']['id']
+        txt = message['message']['text']
+        print("chat_id-->", chat_id)
+        print("txt-->", txt)
+
+        return chat_id, txt
+    except:
+        print("NO text found-->>")
+
+    try:
+        cha_id = message['callback_query']['from']['id']
+        i_txt = message['callback_query']['data']
+        print("cha_id-->", cha_id)
+        print("i_txt-->", i_txt)
+
+        return cha_id, i_txt
+    except:
+        pass
+
+
+# Get the Text message response from the bot
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Image response from the bot by providing the image link
+def tel_send_image(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+    payload = {
+        'chat_id': chat_id,
+        'photo': "https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg"
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Poll response from the bot
+def tel_send_poll(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPoll'
+    payload = {
+        'chat_id': chat_id,
+        "question": "In which direction does the sun rise?",
+        # options are provided in json format
+        "options": json.dumps(["North", "South", "East", "West"]),
+        "is_anonymous": False,
+        "type": "quiz",
+        # Here we are providing the index for the correct option(i.e. indexing starts from 0)
+        "correct_option_id": 2
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Button response in the keyboard section
+def tel_send_button(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    payload = {
+        'chat_id': chat_id,
+        'text': "What is this?",    # button should be in the propper format as described
+        'reply_markup': {
+            'keyboard': [[
+                {
+                    'text': 'supa'
+                },
+                {
+                    'text': 'mario'
+                }
+            ]]
+        }
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Inline button response
+def tel_send_inlinebutton(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    payload = {
+        'chat_id': chat_id,
+        'text': "What is this?",
+        'reply_markup': {
+            "inline_keyboard": [[
+                {
+                    "text": "A",
+                    "callback_data": "ic_A"
+                },
+                {
+                    "text": "B",
+                    "callback_data": "ic_B"
+                }]
+            ]
+        }
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Button response from the bot with the redirected URL
+def tel_send_inlineurl(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    payload = {
+        'chat_id': chat_id,
+        'text': "Which link would you like to visit?",
+        'reply_markup': {
+            "inline_keyboard": [
+                [
+                    {"text": "google", "url": "http://www.google.com/"},
+                    {"text": "youtube", "url": "http://www.youtube.com/"}
+                ]
+            ]
+        }
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Audio response from the bot by providing the URL for the audio
+def tel_send_audio(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendAudio'
+
+    payload = {
+        'chat_id': chat_id,
+        "audio": "http://www.largesound.com/ashborytour/sound/brobob.mp3",
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Document response from the bot by providing the URL for the Document
+def tel_send_document(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendDocument'
+
+    payload = {
+        'chat_id': chat_id,
+        "document": "http://www.africau.edu/images/default/sample.pdf",
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Video response from the bot by providing the URL for the Video
+def tel_send_video(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendVideo'
+
+    payload = {
+        'chat_id': chat_id,
+        "video": "https://www.appsloveworld.com/wp-content/uploads/2018/10/640.mp4",
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the url for the file through the file id
+def tel_upload_file(file_id):
+    # Getting the url for the file
+    url = f'https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}'
+    a = requests.post(url)
+    json_resp = json.loads(a.content)
+    print("json_resp-->", json_resp)
+    file_pathh = json_resp['result']['file_path']
+    print("file_pathh-->", file_pathh)
+
+    # saving the file to our computer
+    url_1 = f'https://api.telegram.org/file/bot{TOKEN}/{file_pathh}'
+    b = requests.get(url_1)
+    file_content = b.content
+    with open(file_pathh, "wb") as f:
+        f.write(file_content)
+
+
+# Reading the respomnse from the user and responding to it accordingly
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        msg = request.get_json()
+        try:
+            chat_id, txt = tel_parse_message(msg)
+            if txt == "hi":
+                tel_send_message(chat_id, "Hello, UPOU!")
+            else:
+                #tel_send_message(chat_id, 'Kamusta po kayo?!')
+                send_email(msg, txt)
+              
+        except:
+            print("fromindex-->")
+
+        return Response('ok', status=200)
+    else:
+        return "<h1>Welcome!</h1>"
+
+      
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
